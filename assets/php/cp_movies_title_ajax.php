e0c4bbd8b59ac988d18481926d81b19feffdd2a1
Jarvl <ajarvis7771@gmail.com>
2015-08-24 17:39:15 -0400

    testing

--- assets/php/cp_movies_title_ajax.php
+++ assets/php/cp_movies_title_ajax.php
@@ -24,6 +24,6 @@
 
     echo '<h1 class="exoextralight">'.$titleText.'</h1>';
 	echo '<hr>';
-    echo var_dump(parseCpMovies("release_status=snatched"));
+    echo var_dump(parseCpMovies("status=done"));
 
 ?>
