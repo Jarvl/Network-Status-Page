881526c1c2a89a2777c88150774506fee6db3df3
Jarvl <ajarvis7771@gmail.com>
2015-08-24 16:27:20 -0400

    Further removing of plex checks

--- index.php
+++ index.php
@@ -221,7 +221,7 @@
 			</div>
 		</div>-->
 		<!-- Invisible php div-->
-		<div id="plex_check"></div>
+		<!--<div id="plex_check"></div>-->
 
 		<!-- javascript
 	    ================================================== -->
@@ -277,7 +277,6 @@
 				});
 
 				// Assign varibles to DOM sections
-				var $plex_check_refresh = $('#plex_check');
 				var $left_column_top_refresh = $('#left_column_top');
 				var $bandwidth_refresh = $('#bandwidth');
 				var $ping_refresh = $('#ping');
@@ -291,7 +290,6 @@
 	        	// Load external php files & assign variables
 	        	$cp_movies_title_refresh.load("assets/php/cp_movies_title_ajax.php");
 	        	$cp_movies_refresh.load("assets/php/cp_movies_ajax.php");
-	        	$plex_check_refresh.load('assets/php/plex_check_ajax.php');
 	        	$left_column_top_refresh.load('assets/php/left_column_top_ajax.php');
 	        	$bandwidth_refresh.load("assets/php/bandwidth_ajax.php");
 	        	$ping_refresh.load("assets/php/ping_ajax.php");
@@ -299,40 +297,40 @@
 	        	$system_load_refresh.load("assets/php/system_load_ajax.php");
 	        	$system_ram_refresh.load("assets/php/system_ram_ajax.php");
 	        	$disk_space_refresh.load("assets/php/disk_space_ajax.php");
-
-				var refreshIdfastest = setInterval(function(){
-			        		$plex_check_refresh.load('assets/php/plex_check_ajax.php');
-			        	}, 10000); // at 3 & 5 seconds python was crashing.
-
-			        	var refreshIdfastest = setInterval(function(){
-			            	$system_load_refresh.load('assets/php/system_load_ajax.php');
-			        	}, 5000); // 5 seconds
-
-			        	var refreshIdfastest = setInterval(function(){
-			            	$system_ram_refresh.load('assets/php/system_ram_ajax.php');
-			        	}, 5000); // 5 seconds
-
-			        	var refreshId30 = setInterval(function(){
-			        		$bandwidth_refresh.load("assets/php/bandwidth_ajax.php");
-			        		$ping_refresh.load("assets/php/ping_ajax.php");
-			        		$services_refresh.load("assets/php/services_ajax.php");
-			        	}, 10000); // 10 seconds
 /*
-			        	var refreshId60 = setInterval(function(){
-			        		$transcodeSessions.load("assets/php/transcode_sessions_ajax.php");
-			        	}, 60000); // 60 seconds
+				var refreshIdfastest = setInterval(function(){
+	        		$plex_check_refresh.load('assets/php/plex_check_ajax.php');
+	        	}, 10000); // at 3 & 5 seconds python was crashing.
 */
-			        	var refreshIdslow = setInterval(function(){
-			            	$disk_space_refresh.load('assets/php/disk_space_ajax.php');
-			        	}, 120000); // 2 minutes
+	        	var refreshIdfastest = setInterval(function(){
+	            	$system_load_refresh.load('assets/php/system_load_ajax.php');
+	        	}, 5000); // 5 seconds
 
-			        	var refreshtopleft = setInterval(function(){
-			            	$left_column_top_refresh.load('assets/php/left_column_top_ajax.php');
-			        	}, 300000); // 5 minutes
+	        	var refreshIdfastest = setInterval(function(){
+	            	$system_ram_refresh.load('assets/php/system_ram_ajax.php');
+	        	}, 5000); // 5 seconds
 
-			        	// Load these sections only if Plex has changed states
-			        	var theResource = "assets/caches/plexcheckfile2.txt";
+	        	var refreshId30 = setInterval(function(){
+	        		$bandwidth_refresh.load("assets/php/bandwidth_ajax.php");
+	        		$ping_refresh.load("assets/php/ping_ajax.php");
+	        		$services_refresh.load("assets/php/services_ajax.php");
+	        	}, 10000); // 10 seconds
+/*
+	        	var refreshId60 = setInterval(function(){
+	        		$transcodeSessions.load("assets/php/transcode_sessions_ajax.php");
+	        	}, 60000); // 60 seconds
+*/
+	        	var refreshIdslow = setInterval(function(){
+	            	$disk_space_refresh.load('assets/php/disk_space_ajax.php');
+	        	}, 120000); // 2 minutes
 
+	        	var refreshtopleft = setInterval(function(){
+	            	$left_column_top_refresh.load('assets/php/left_column_top_ajax.php');
+	        	}, 300000); // 5 minutes
+
+	        	// Load these sections only if Plex has changed states
+	        	//var theResource = "assets/caches/plexcheckfile2.txt";
+/*
  				var refreshconditional = setInterval(function(){
 				          if(localStorage["resourcemodified"]) {
 				               $.ajax({
@@ -361,9 +359,9 @@
 				               })
 				          }
 				}, 5000); // 5 seconds
-
+*/
 				// Change the size of the now playing div to match the client size
-				function doResizeNowPlaying() {
+				function doResizeCpMovies() {
 					var height = 0;
 					var body = window.document.body;
 					if (window.innerHeight) {
@@ -376,19 +374,19 @@
 					cp_movies.style.height = ((height - cp_movies.offsetTop) + "px");
 					console.log("Div resize complete. New size is: " + height);
 				};
-				// Detect if we are on a mobile device, if we aren't resize the now playing div using doResizeNowPlaying()
+				// Detect if we are on a mobile device, if we aren't resize the cp movies div using doResizeCpMovies()
 				if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 					// some code..
 				} else {
 					var resizeTimer;
 					$(window).resize(function() {
 						clearTimeout(resizeTimer);
-						resizeTimer = setTimeout(doResizeNowPlaying, 100);
+						resizeTimer = setTimeout(doResizeCpMovies, 100);
 					});
 					// Resize the now playing div 5 seconds after page load
 					$(function(){
 	   					clearTimeout(resizeTimer);
-						resizeTimer = setTimeout(doResizeNowPlaying, 5000);
+						resizeTimer = setTimeout(doResizeCpMovies, 5000);
 					});
 				}
 		    	});
